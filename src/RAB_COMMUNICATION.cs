9ef85ac9da6fd17a7f560cc15aca46b88ed2bee3
Vlad <Vlad.Levyant@zaha-hadid.com>
2024-08-05 10:43:07 +0100

    Clean Up

--- src/RAB_COMMUNICATION.cs
+++ src/RAB_COMMUNICATION.cs
@@ -36,6 +36,7 @@ struct Rotation
         w = _w;
     }
 
+    // Hardcoded for now
     public static Rotation Default => new Rotation(0.0f, 1.0f, 0.0f, 0.0f);
 }
 
@@ -58,6 +59,7 @@ struct Target
 
     public override String ToString()
     {
+        // Hardcoded for now
         return $"robtarget;[[{pos.x},{pos.y},{pos.z}],[{ort.x},{ort.y},{ort.z},{ort.w}],[0,0,0,0],[9E9,9E9,9E9,9E9,9E9,9E9]]";
     }
 }
@@ -99,6 +101,7 @@ struct TargetSpeedData
     }
     public override String ToString()
     {
+        // Hardcoded for now
         return $"target_speed_data;[[[{pos.x},{pos.y},{pos.z}],[{ort.x},{ort.y},{ort.z},{ort.w}],[0,0,0,0],[9E9,9E9,9E9,9E9,9E9,9E9]],[{speed_data.tcp}, {speed_data.ori}, {speed_data.leax}, {speed_data.reax}]]";
     }
 }
@@ -106,9 +109,9 @@ struct TargetSpeedData
 class RAB_COM
 {
     public Controller controller;
-    public IpcQueue ROB_Queue;
+    public IpcQueue Robot_Queue;
 
-    public IpcQueue TheOneAndOnlyQueue;
+    public IpcQueue SDK_Queue;
 
     public IpcMessage sendPositionMessage;
 
@@ -131,26 +134,24 @@ class RAB_COM
         comm.controller = GetController(NetworkScannerSearchCriterias.Real);
 
         //get T_ROB1 queue to send msgs to RAPID task
-        comm.ROB_Queue = comm.controller.Ipc.GetQueue("RMQ_T_ROB1");
-        //create my own PC SDK queue to receive msgs
-
+        comm.Robot_Queue = comm.controller.Ipc.GetQueue("RMQ_T_ROB1");
         comm.MessageSize = Ipc.MaxMessageSize;
 
-        if (!comm.controller.Ipc.Exists("ONLY_Q"))
+        if (!comm.controller.Ipc.Exists("RMQ_SDK"))
         {
-            comm.TheOneAndOnlyQueue = comm.controller.Ipc.CreateQueue("ONLY_Q", 10, comm.MessageSize);
+            comm.SDK_Queue = comm.controller.Ipc.CreateQueue("RMQ_SDK", 10, comm.MessageSize);
         }
         else
         {
-            comm.controller.Ipc.DeleteQueue(comm.controller.Ipc.GetQueueId("ONLY_Q"));
-            comm.TheOneAndOnlyQueue = comm.controller.Ipc.CreateQueue("ONLY_Q", 10, comm.MessageSize);
-            //comm.TheOneAndOnlyQueue = comm.controller.Ipc.GetQueue("ONLY_Q");
+            // If the queue already exists, delete it and recreate it
+            comm.controller.Ipc.DeleteQueue(comm.controller.Ipc.GetQueueId("RMQ_SDK"));
+            comm.SDK_Queue = comm.controller.Ipc.CreateQueue("RMQ_SDK", 10, comm.MessageSize);
         }
 
-        var queue_name = comm.TheOneAndOnlyQueue.Name;
-        var queue_id = comm.TheOneAndOnlyQueue.QueueId;
-        var capacity = comm.TheOneAndOnlyQueue.Capacity;
-        var messagesize = comm.TheOneAndOnlyQueue.MessageSizeLimit;
+        var queue_name = comm.SDK_Queue.Name;
+        var queue_id = comm.SDK_Queue.QueueId;
+        var capacity = comm.SDK_Queue.Capacity;
+        var messagesize = comm.SDK_Queue.MessageSizeLimit;
 
         Console.WriteLine($"Queue {queue_name}:{queue_id}:{capacity}:{messagesize}");
 
@@ -179,15 +180,15 @@ class RAB_COM
         //comm.SendMessage(new CustomDataTest(1809, -166, 1248, 150, 150, 5000, 1000));
         var send_mes = DateTime.Now;
 
-        comm.CheckReturnMsg(); //Target Acquired
+        comm.CheckReturnMsg();
         var rec_mes = DateTime.Now;
 
         Console.WriteLine($"That took: {rec_mes-send_mes}");
 
-        comm.CheckReturnMsg(); //Here boss
+        comm.CheckReturnMsg();
 
         //comm.SendMessage(false);
-        //comm.CheckReturnMsg(); //Bye
+        //comm.CheckReturnMsg();
 
         Console.ReadLine();
     }
@@ -210,6 +211,7 @@ class RAB_COM
         var yes = "bool;TRUE\0";
         var no = "bool;FALSE\0";
 
+
         //Create message data
         if (boolMsg)
         {
@@ -226,21 +228,19 @@ class RAB_COM
 
         //Place data and sender information in message
         sendExitMessage.SetData(data);
-        sendExitMessage.Sender = TheOneAndOnlyQueue.QueueId;
+        sendExitMessage.Sender = SDK_Queue.QueueId;
         //Send message to the RAPID queue
-        ROB_Queue.Send(sendExitMessage);
+        Robot_Queue.Send(sendExitMessage);
     }
 
     public void SendMessage(Point point, Rotation rot)
     {
         sendPositionMessage = new IpcMessage();
 
-        //String string_to_send = $"robtarget;[[{point.x},{point.y},{point.z}],[0,1,0,0],[0,0,0,0],[9E9,9E9,9E9,9E9,9E9,9E9]]";
-        //String string_to_send = $"robtarget;[[{point.x.ToString().PadLeft(4, '0')},{point.y.ToString().PadLeft(4, '0')},{point.z.ToString().PadLeft(4, '0')}],[0,1,0,0],[0,0,0,0],[9E9,9E9,9E9,9E9,9E9,9E9]]";
-
         String string_to_send = $"robtarget;[[{point.x},{point.y},{point.z}],[0,1,0,0],[0,0,0,0],[9E9,9E9,9E9,9E9,9E9,9E9]]";
         int PaddingSize = MessageSize - string_to_send.Length;
 
+        //Pad message to max size
         string_to_send = string_to_send.PadRight(PaddingSize, ' ');
 
         Console.WriteLine(string_to_send);      
@@ -249,10 +249,10 @@ class RAB_COM
         Console.WriteLine($"target Length:  {data.GetLength(0)}" );
         //Place data and sender information in message
         sendPositionMessage.SetData(data);
-        
-        sendPositionMessage.Sender = TheOneAndOnlyQueue.QueueId;
+
+        sendPositionMessage.Sender = SDK_Queue.QueueId;
         //Send message to the RAPID queue
-        ROB_Queue.Send(sendPositionMessage);
+        Robot_Queue.Send(sendPositionMessage);
     }
 
     //public void SendMessage(CustomDataTest custom_data)
@@ -287,7 +287,7 @@ class RAB_COM
         do
         {
             Console.WriteLine("Trying to get message");
-            ret = TheOneAndOnlyQueue.Receive(timeout, returnMessage);
+            ret = SDK_Queue.Receive(timeout, returnMessage);
         } while (ret != IpcReturnType.OK);
 
         var string_length = returnMessage.UserDef+9;
